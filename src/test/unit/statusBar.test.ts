df42b78bb8a64d0b031eae08be8d75944d540c20
Gil Cleber Barboza <gil.cleber@gmail.com>
2026-09-05 17:25:00 -0300

    test(statusBar): labels pt-br nos tooltips (aprovados/falhas/erros/pulados)

--- src/test/unit/statusBar.test.ts
+++ src/test/unit/statusBar.test.ts
@@ -9,7 +9,7 @@ test('formatResults: todos pass', () => {
   assert.strictEqual(fmt.icon, '$(testing-passed)');
   assert.match(fmt.text, /4\/4/);
   assert.match(fmt.text, /2\.5s/);
-  assert.match(fmt.tooltip, /4 passed/);
+  assert.match(fmt.tooltip, /4 aprovados/);
 });
 
 test('formatResults: com falhas', () => {
@@ -17,23 +17,23 @@ test('formatResults: com falhas', () => {
   assert.strictEqual(fmt.icon, '$(testing-failed)');
   assert.match(fmt.text, /3\/5/);
   assert.match(fmt.text, /5\.1s/);
-  assert.match(fmt.tooltip, /3 passed/);
-  assert.match(fmt.tooltip, /2 failed/);
+  assert.match(fmt.tooltip, /3 aprovados/);
+  assert.match(fmt.tooltip, /2 falhas/);
 });
 
 test('formatResults: com erros', () => {
   const fmt = formatResults(0, 0, 0, 2, 1000);
   assert.strictEqual(fmt.icon, '$(testing-failed)');
   assert.match(fmt.text, /0\/2/);
-  assert.match(fmt.tooltip, /2 errored/);
+  assert.match(fmt.tooltip, /2 erros/);
 });
 
 test('formatResults: com skipped', () => {
   const fmt = formatResults(5, 1, 2, 0, 3000);
   assert.strictEqual(fmt.icon, '$(testing-failed)');
   assert.match(fmt.text, /5\/8/);
-  assert.match(fmt.tooltip, /5 passed/);
-  assert.match(fmt.tooltip, /2 skipped/);
+  assert.match(fmt.tooltip, /5 aprovados/);
+  assert.match(fmt.tooltip, /2 pulados/);
 });
 
 test('formatResults: tooltip com duracao', () => {
